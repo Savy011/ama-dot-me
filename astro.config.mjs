d9f23c42fcc2074f6729aac260802bf066d24477
Savy <savy.novachez011@proton.me>
2026-04-20 22:46:30 +0530

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
+
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [htmx()],
+  adapter: cloudflare(),
 });
