488dcaedbda085eb4df3b4d8e5e0bd5f2f38859f
Savy <savy.novachez011@proton.me>
2026-04-21 17:35:29 +0530

    add worker bindings, update pkg scripts, setup htmx manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@
 
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
-import htmx from "astro-htmx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [htmx()],
   adapter: cloudflare(),
 });
