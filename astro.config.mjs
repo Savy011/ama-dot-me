d0d0a89374faf68826ad5d0b97c117ce46083d47
Savy <savy.novachez011@proton.me>
2026-04-26 17:13:58 +0530

    setup stylings, add header, setup monospace font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Atkinson Hyperlegible Mono",
+    cssVariable: "--font-mono"
+  }],
 });
